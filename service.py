d6522ada45ba7fe573ac0aff68bbeed7a1c20223
Antonio Lopez <antdlopma@gmail.com>
2017-12-20 16:24:18 +0100

    Added more code to add/rm users

--- service.py
+++ service.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import jsonify
 from flask import render_template
+from flask import request
 from flask_mysqldb import MySQL
 import MySQLdb
 
@@ -9,7 +10,7 @@ app = Flask(__name__)
 conn = MySQLdb.connect(host="localhost",
                        user = "root",
                        passwd = "root",
-                       db = "farmacias")
+                       db = "pharmaciesDB")
 cursor = conn.cursor()
 
 @app.route("/")
@@ -28,11 +29,24 @@ def login():
 
 @app.route("/registry")
 def registry():
-    return jsonify(login="success")
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO users (name,password,client) VALUES("'+name+'","'+password+'","'+client+'")")
+    data = cursor.fetchone()
+    if data is None:
+        return jsonify(registry="fail")
+    else:
+        return jsonify(registry="success")
 
 @app.route("/rmUser")
 def rmUser():
-    return jsonify(login="success")
+    id=1;
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM users where id="+id);
+    data = cursor.fetchone()
+    if data is None:
+        return jsonify(removeUser="fail")
+    else:
+        return jsonify(removeUser="success")
 
 @app.route("/addProduct")
 def addProduct():
@@ -46,6 +60,10 @@ def modifyProduct():
 def deleteProduct():
     return jsonify(login="success")
 
+@app.route("/doRequest")
+def doRequest():
+    return jsonify(login="success")
+
 @app.route("/getCatalog")
 def getCatalog():
     return jsonify(login="success")
@@ -54,9 +72,13 @@ if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
 
 
-
 # apt-get python-dev mysql_server libmysqlclient-dev
 # pip install mysqlclient flask flask-mysqldb
 #
+# CREATE DATABASE pharmaciesDB
+#
 # CREATE TABLE users ( id INT NULL AUTO_INCREMENT, name VARCHAR(45) NULL,
-# password VARCHAR(45) NULL, PRIMARY KEY (id));
+# password VARCHAR(45) NULL, client tinyint(1) ,PRIMARY KEY (id));
+#
+# CREATE TABLE catalog ( id INT NULL AUTO_INCREMENT, name VARCHAR(45) NULL,
+# quantity INT NULL, price INT NULL, PRIMARY KEY (id));
