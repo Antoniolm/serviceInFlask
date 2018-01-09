9a69664cdab63c2414de498c279ef39abcc0bc3a
Antonio Lopez <antdlopma@gmail.com>
2018-01-10 00:07:48 +0100

    Update the service

--- service.py
+++ service.py
@@ -3,9 +3,13 @@ from flask import jsonify
 from flask import render_template
 from flask import request
 from flask_mysqldb import MySQL
+import md5
 import MySQLdb
 import json
 import os
+
+def dbConnection():
+    return MySQLdb.connect(host="localhost", user = "root", passwd = "root", db = "pharmaciesDB")
 
 app = Flask(__name__)
 
@@ -25,18 +29,18 @@ if data[0]==0:
     os.system("mysql -u root -proot < dbSchema.sql ")
 else: #Si lo esta
     db.close();
-    db = MySQLdb.connect(host="localhost",
-                         user = "root",
-                         passwd = "root",
-                         db = "pharmaciesDB")
+db = dbConnection()
 
 @app.route("/")
 def root():
+    db = dbConnection()
+
     cursor = db.cursor()
     cursor.execute("select count(*) from information_schema.SCHEMATA where schema_name not in ('mysql','information_schema');")
     data = cursor.fetchone()
-    print(data[0])
-    return "yeah"
+    db.close()
+
+    return render_template('index.html',error=None)
 
 ########################
 ## Users
@@ -44,29 +48,34 @@ def root():
 
 @app.route("/users", methods=['GET', 'POST'])
 def users():
+    db = dbConnection()
+
     if request.method == 'GET':
         jsonResult= jsonify()
         cursor = db.cursor()
-        rows=cursor.execute("SELECT name,password,client FROM users")
+        rows=cursor.execute("SELECT id,name,password,client FROM users")
 
         columns = cursor.description
         result = [{columns[index][0]:column for index, column in enumerate(value)}   for value in cursor.fetchall()]
+        db.close()
+
         return jsonify(result);
 
     if request.method == 'POST':
         if request.json:
             content= request.get_json()
             username = content.get('name')
-            password = content.get('password')
+            password = md5.new(content.get('password')).hexdigest()
             client = int(content.get('client'))
         else :
             username = request.form['name']
-            password = request.form['password']
+            password = md5.new(request.form['password']).hexdigest()
             client = int(request.form['client'])
 
         cursor = db.cursor()
         data = cursor.execute("INSERT INTO users (name,password,client) VALUES(%s,%s,%s)",(username,password,client))
         db.commit()
+        db.close()
 
         return jsonify(registry="success")
 
@@ -76,31 +85,35 @@ def users():
 
 @app.route("/users/<idUser>", methods=['GET', 'POST' , 'DELETE'])
 def user(idUser):
+    db = dbConnection()
+
     if request.method == 'GET':
         cursor = db.cursor()
 
         cursor.execute("SELECT * from users where id=%s", idUser)
         data = cursor.fetchone()
+        db.close()
 
         if data is None:
             return jsonify(error="UsernotFound")
         else:
-            return jsonify(name=data[1],password=data[2],client=data[3])
+            return jsonify(id=data[0],name=data[1],password=data[2],client=data[3])
 
     if request.method == 'POST':
         if request.json:
             content= request.get_json()
             username = content.get('name')
-            password = content.get('password')
+            password = md5.new(content.get('password')).hexdigest()
             client = int(content.get('client'))
         else :
             username = request.form['name']
-            password = request.form['password']
+            password = md5.new(request.form['password']).hexdigest()
             client = int(request.form['client'])
 
         cursor = db.cursor()
         data = cursor.execute("UPDATE users SET name=%s, password=%s, client=%s WHERE id=%s", (username,password,client,idUser))
         db.commit()
+        db.close()
 
         return jsonify(registry="success")
 
@@ -108,6 +121,7 @@ def user(idUser):
         cursor = db.cursor()
         cursor.execute("DELETE FROM users where id=%s", idUser);
         db.commit()
+        db.close()
 
         return jsonify(removeUser="success")
     return jsonify(error="DontMethodDetected")
@@ -116,18 +130,21 @@ def user(idUser):
 
 @app.route("/login", methods=['POST'])
 def login():
+    db = dbConnection()
+
     if request.json:
         content= request.get_json()
         username = content.get('name')
-        password = content.get('password')
+        password = md5.new(content.get('password')).hexdigest()
     else :
         username = request.form['name']
-        password = request.form['password']
+        password = md5.new(request.form['password']).hexdigest()
 
     cursor = db.cursor()
 
     cursor.execute("SELECT * from users where name=%s and password=%s",(username,password))
     data = cursor.fetchone()
+    db.close()
 
     if data is None:
         return jsonify(login="fail")
@@ -140,13 +157,17 @@ def login():
 
 @app.route("/products", methods=['GET', 'POST' , 'DELETE'])
 def products():
+    db = dbConnection()
+
     if request.method == 'GET':
         jsonResult= jsonify()
         cursor = db.cursor()
-        rows=cursor.execute("SELECT name,quantity,price FROM catalog")
+        rows=cursor.execute("SELECT id,name,quantity,price FROM catalog")
 
         columns = cursor.description
         result = [{columns[index][0]:column for index, column in enumerate(value)}   for value in cursor.fetchall()]
+        db.close()
+
         return jsonify(result);
 
     if request.method == 'POST':
@@ -163,6 +184,7 @@ def products():
         cursor = db.cursor()
         cursor.execute("INSERT INTO catalog (name,quantity,price) VALUES(%s,%s,%s)", (name,quantity,price))
         db.commit()
+        db.close()
 
         return jsonify(addProduct="success")
 
@@ -172,16 +194,19 @@ def products():
 
 @app.route("/products/<idProduct>", methods=['GET', 'POST' , 'DELETE'])
 def product(idProduct):
+    db = dbConnection()
+
     if request.method == 'GET':
         cursor = db.cursor()
 
         cursor.execute("SELECT * from catalog where id=%s", idProduct)
         data = cursor.fetchone()
+        db.close()
 
         if data is None:
             return jsonify(error="ProductnotFound")
         else:
-            return jsonify(name=data[1],quantity=data[2],price=data[3])
+            return jsonify(id=data[0],name=data[1],quantity=data[2],price=data[3])
 
     if request.method == 'POST':
         if request.json:
@@ -197,6 +222,7 @@ def product(idProduct):
         cursor = db.cursor()
         data = cursor.execute("UPDATE catalog SET name=%s, quantity=%s, price=%s WHERE id=%s", (name,quantity,price,idProduct))
         db.commit()
+        db.close()
 
         return jsonify(addProduct="success")
 
@@ -204,15 +230,30 @@ def product(idProduct):
         cursor = db.cursor()
         cursor.execute("DELETE FROM catalog where id=%s", idProduct);
         db.commit()
+        db.close()
 
         return jsonify(removeProduct="success")
     return jsonify(error="DontMethodDetected")
 
 ###################################################################
 
-@app.route("/doRequest", methods=['GET', 'POST'])
-def doRequest():
-    return jsonify(login="success")
+@app.route("/doOrder", methods=['POST'])
+def doOrder():
+    db = dbConnection()
+
+    if request.json:
+        content= request.get_json()
+        print(len(content))
+        print(content[0].get('prueba'))
+
+    cursor = db.cursor()
+    for i in range(0, len(content)):
+        data = cursor.execute("UPDATE catalog SET quantity=%s WHERE id=%s", (content[i].get('quantity'),content[i].get('id')))
+        db.commit()
+
+    db.close()
+
+    return jsonify(request="success")
 
 ########################
 ## Pharmacies
@@ -220,13 +261,17 @@ def doRequest():
 
 @app.route("/pharmacies", methods=['GET', 'POST' , 'DELETE'])
 def pharmacies():
+    db = dbConnection()
+
     if request.method == 'GET':
         jsonResult= jsonify()
         cursor = db.cursor()
-        rows=cursor.execute("SELECT name,latitude,longitude FROM pharmacies")
+        rows=cursor.execute("SELECT id,name,latitude,longitude FROM pharmacies")
 
         columns = cursor.description
         result = [{columns[index][0]:column for index, column in enumerate(value)}   for value in cursor.fetchall()]
+        db.close()
+
         return jsonify(result);
 
     if request.method == 'POST':
@@ -243,6 +288,7 @@ def pharmacies():
         cursor = db.cursor()
         cursor.execute("INSERT INTO pharmacies (name,latitude,longitude) VALUES(%s,%s,%s)", (name,latitude,longitude))
         db.commit()
+        db.close()
 
         return jsonify(addPharmacies="success")
 
@@ -252,16 +298,19 @@ def pharmacies():
 
 @app.route("/pharmacies/<idPharma>", methods=['GET', 'POST' , 'DELETE'])
 def pharmacy(idPharma):
+    db = dbConnection()
+
     if request.method == 'GET':
         cursor = db.cursor()
 
         cursor.execute("SELECT * from pharmacies where id=%s", idPharma)
         data = cursor.fetchone()
+        db.close()
 
         if data is None:
             return jsonify(error="PharmacynotFound")
         else:
-            return jsonify(name=data[1],latitude=data[2],longitude=data[3])
+            return jsonify(id=data[0],name=data[1],latitude=data[2],longitude=data[3])
 
     if request.method == 'POST':
         if request.json:
@@ -277,6 +326,7 @@ def pharmacy(idPharma):
         cursor = db.cursor()
         data = cursor.execute("UPDATE pharmacies SET name=%s, latitude=%s, longitude=%s WHERE id=%s", (name,latitude,longitude,idPharma))
         db.commit()
+        db.close()
 
         return jsonify(updatePharmacy="success")
 
@@ -284,8 +334,10 @@ def pharmacy(idPharma):
         cursor = db.cursor()
         cursor.execute("DELETE FROM pharmacies where id=%s", idPharma);
         db.commit()
+        db.close()
 
         return jsonify(removePharmacy="success")
+
     return jsonify(error="DontMethodDetected")
 
 ###################################################################
@@ -295,6 +347,8 @@ if __name__ == '__main__':
 #curl -X POST localhost:5000/users/0 -d nm=post2 -d pass=sip -d client=0
 #curl -X DELETE localhost:5000/users/6
 #curl localhost:5000/users/2
+#curl -H "Content-Type: application/json" -X POST -d '{"name":"antonio1","password":"prueba","client":"0"}' https://apidssantoniolm.azurewebsites.net/users
+#curl -H "Content-Type: application/json" -X POST -d '[{"id":"1","quantity":"0"},{"id":"2","quantity":"5"},{"id":"3","quantity":"30"}]' localhost:5000/requestProduct
 
 # sudo docker build -t antoniolm/prueba .
 # sudo docker run -p 5000:5000 -t antonio/prueba
